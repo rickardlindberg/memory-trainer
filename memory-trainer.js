ce7c6f926f9bb8d577463ef7045779617f4e5bb8
Rickard Lindberg <ricli85@gmail.com>
2013-09-07 10:46:30 +0200

    shows first word in slideshow

---
+++ memory-trainer.js
@@ -0,0 +1,5 @@
+function SlideshowPage(slideshowView, words) {
+    this.start = function () {
+        slideshowView.displayWord(words[0]);
+    };
+}
--- tests/SlideshowPageSpec.js
+++ tests/SlideshowPageSpec.js
@@ -1,7 +1,13 @@
 describe("slideshow page", function () {
 
-    it("shows words", function () {
-        expect(true).toBe(false);
+    it("shows first word when started", function () {
+        var slideshowView = {
+            displayWord: function () {}
+        };
+        spyOn(slideshowView, "displayWord");
+        var slideshowPage = new SlideshowPage(slideshowView, ["monkey", "fan", "flower"]);
+        slideshowPage.start();
+        expect(slideshowView.displayWord).toHaveBeenCalledWith("monkey");
     });
 
 });
