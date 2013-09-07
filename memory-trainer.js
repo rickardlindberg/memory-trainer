01f1a77199ef9d4b4cdfe736a6db2df1721f43bf
Rickard Lindberg <ricli85@gmail.com>
2013-09-07 11:35:11 +0200

    can fade words

--- memory-trainer.js
+++ memory-trainer.js
@@ -1,16 +1,28 @@
 function SlideshowPage(slideshowView) {
+
     this.start = function(settings) {
         slideshowView.displayWord(settings.words[0]);
+        setTimeout(function() {
+            slideshowView.fadeOutWord(500 * settings.secondsPerWord);
+        }, 500 * settings.secondsPerWord);
         setTimeout(function() {
             slideshowView.displayWord(settings.words[1]);
         }, 1000 * settings.secondsPerWord);
     };
+
 }
 
 function SlideshowView() {
+
     this.displayWord = function(word) {
         $("#slideshow .word").html("<b>" + word + "</b>");
+        $("#slideshow .word").fadeIn(0);
     };
+
+    this.fadeOutWord = function(inMs) {
+        $("#slideshow .word").fadeOut(inMs);
+    };
+
 }
 
 $(document).ready(function() {
--- tests/SlideshowPageSpec.js
+++ tests/SlideshowPageSpec.js
@@ -4,11 +4,12 @@ describe("slideshow page", function() {
 
     beforeEach(function() {
         slideshowView = new SlideshowView();
+        spyOn(slideshowView, "displayWord");
+        spyOn(slideshowView, "fadeOutWord");
         slideshowPage = new SlideshowPage(slideshowView);
     });
 
     it("shows first word when started", function() {
-        spyOn(slideshowView, "displayWord");
         slideshowPage.start({
             words: ["monkey", "fan", "flower"],
             secondsPerWord: 6
@@ -23,16 +24,25 @@ describe("slideshow page", function() {
         });
 
         it("switches after the configured number of seconds", function() {
-            spyOn(slideshowView, "displayWord");
             slideshowPage.start({
                 words: ["monkey", "fan"],
                 secondsPerWord: 6
             });
-            expect(slideshowView.displayWord).not.toHaveBeenCalledWith("fan");
             jasmine.Clock.tick(5999);
             expect(slideshowView.displayWord).not.toHaveBeenCalledWith("fan");
             jasmine.Clock.tick(1);
             expect(slideshowView.displayWord).toHaveBeenCalledWith("fan");
+        });
+
+        it("switches fades out after half the time", function() {
+            slideshowPage.start({
+                words: ["monkey", "fan"],
+                secondsPerWord: 6
+            });
+            jasmine.Clock.tick(2999);
+            expect(slideshowView.fadeOutWord).not.toHaveBeenCalled();
+            jasmine.Clock.tick(1);
+            expect(slideshowView.fadeOutWord).toHaveBeenCalledWith(3000);
         });
 
     });
